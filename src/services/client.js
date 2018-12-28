7fa7ffe2ff72adb3269980de7a0f9c6b6e288d2f
Ilya Shapkin <ilya@shapkin.org>
2018-12-28 22:47:27 +0300

    fixiki part2

--- src/services/client.js
+++ src/services/client.js
@@ -4,10 +4,6 @@ import './auth'
 
 let token = localStorage.getItem('token');
 
-if(!token) {
-    location.reload()
-}
-
 let client = axios.create({
     baseURL: config.cutwise.baseUrl,
     headers: { 'Authorization': 'Bearer ' + token }
