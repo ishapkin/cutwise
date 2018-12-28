d12a4b0e7da7c2d8960e5bb87ae3b89ed57d91b3
Ilya Shapkin <ilya@shapkin.org>
2018-12-29 00:53:39 +0300

    toggle icon

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
       <div class="container">
         <router-link class="navbar-brand" to="/">Cutwise API test</router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
       </div>
     </nav>
     <router-view/>
